b5922b3e767b3974343cb02ceed1a19c6b3359a7
Sflaton <suzanne.flaton@gmail.com>
2016-05-10 16:01:19 +0200

    add TDD models Category and Movie

---
+++ spec/factories/movies.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :movie do
+    title "MyString"
+    description "MyText"
+  end
+end
---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Movie, type: :model do
+  it{ is_expected.to belong_to :category}
+  it{ is_expected.to validate_presence_of(:title)}
+end
