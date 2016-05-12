fc9d2152554e821b58880f759d292cbf7b826180
Sflaton <suzanne.flaton@gmail.com>
2016-05-12 11:01:28 +0200

    view index and show categories

--- spec/views/categories/show.html.erb_spec.rb
+++ spec/views/categories/show.html.erb_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "categories/index.html.erb", type: :view do
+RSpec.describe "categories/show.html.erb", type: :view do
   let(:category) { FactoryGirl.create(:category)}
+  let(:movies) {FactoryGirl.create(:movie, category: category)}
   before do
     assign(:category, category)
     render
   end
 
-  it "shows a title" do
+  it "shows a category name" do
     assert_select "h2", text: category.name, count: 1
-
   end
+  
 end
